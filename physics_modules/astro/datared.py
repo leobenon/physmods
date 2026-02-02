e0969c82d13afa7fccfab66223a2b55bd3123e9a
leobenon <rukan.unal@gmail.com>
2026-02-02 20:09:11 +0100

    Modified datared.py

--- physics_modules/astro/datared.py
+++ physics_modules/astro/datared.py
@@ -1,3 +1,27 @@
 import numpy as mp
 from astropy.io import fits
 import matplotlib.pyplot as plt
+
+
+def load_image(filepath):
+    """
+    Loading the fits data
+    ----------
+    Parameters
+    -------------
+    filepath: 
+        Path of the fits file.
+    -----------
+    Returns
+    ---------
+    data:
+        Image data
+    exposure_time: 
+        exposure time of the image
+    """
+    with fits.open(filepath) as hdul:
+        header = hdul[0].header
+        exposure_time = header["EXPOSURE"] 
+        data = hdul[0].data
+    return data, exposure_time
+
